ab3cb670307d5a0565bb463740ee456548dad118
Dilion <antoine.eugenie@ynov.com>
2025-02-17 14:41:01 +0100

    controller

---
+++ src/Controller/Registercontroller.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RegisterController extends AbstractController
+{
+    public function index(): Response
+    {
+        $test = "chose";
+        return $this->render('index.html.twig', ["test" => $test]);
+    }
+}
