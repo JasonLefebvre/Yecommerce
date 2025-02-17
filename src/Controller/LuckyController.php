b5fcf15f60816d1d3d99c9a523919141b35d2f03
JasonLefebvre <jasonlefebvre25septembre@gmail.com>
2025-02-17 11:47:14 +0100

    routes

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response;
+class LuckyController
+{
+    public function number():Response
+    {
+        $number = rand(1, 100);
+
+        return new Response('<html><body>Lucky number: '.$number.'</body></html>');
+    }
+}
