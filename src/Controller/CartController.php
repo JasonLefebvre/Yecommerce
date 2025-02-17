6f969c70c22b9d70337626af834f9399c336a525
JasonLefebvre <jasonlefebvre25septembre@gmail.com>
2025-02-17 14:09:39 +0100

    initialisation des classes des controlleurs

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -1,1 +1,16 @@
 <?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CartController extends AbstractController
+{
+    public function index(): Response
+    {
+        $test = "chose";
+        return $this->render('index.html.twig', ["test" => $test]);
+    }
+}
--- src/Controller/EditController.php
+++ src/Controller/EditController.php
@@ -1,1 +1,17 @@
 <?php
+
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class EditController extends AbstractController
+{
+    public function index(): Response
+    {
+        $test = "chose";
+        return $this->render('index.html.twig', ["test" => $test]);
+    }
+}
