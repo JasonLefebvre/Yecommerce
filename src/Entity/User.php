297971ed568d8bde43178a624179bdd87cb4b3f0
Dilion <antoine.eugenie@ynov.com>
2025-02-18 13:06:30 +0100

    Update User.php

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,31 +4,41 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-class User implements \Stringable
+#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
+class User implements UserInterface, PasswordAuthenticatedUserInterface, \Stringable
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $username = null;
+    #[ORM\Column(length: 180)]
+    private ?string $email = null;
 
-    #[ORM\Column(length: 255)]
+    /**
+     * @var list<string> The user roles
+     */
+    #[ORM\Column]
+    private array $roles = [];
+
+    /**
+     * @var string The hashed password
+     */
+    #[ORM\Column]
     private ?string $password = null;
 
     #[ORM\Column(length: 255)]
-    private ?string $email = null;
-    #[ORM\Column]
-    private ?float $solde = null;
+    private ?string $username = null;
 
-    #[ORM\Column(length: 255, nullable: true)]
-    private ?string $photoProfil = null;
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
 
-    #[ORM\Column(length: 50)]
-    private ?string $role = null;
     public function getEmail(): ?string
     {
         return $this->email;
@@ -41,42 +51,43 @@ class User implements \Stringable
         return $this;
     }
 
-    public function getSolde(): ?float
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
     {
-        return $this->solde;
+        return (string) $this->email;
     }
 
-    public function setSolde(float $solde): static
+    /**
+     * @see UserInterface
+     *
+     * @return list<string>
+     */
+    public function getRoles(): array
     {
-        $this->solde = $solde;
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    /**
+     * @param list<string> $roles
+     */
+    public function setRoles(array $roles): static
+    {
+        $this->roles = $roles;
 
         return $this;
     }
 
-    public function getPhotoProfil(): ?string
-    {
-        return $this->photoProfil;
-    }
-
-    public function setPhotoProfil(?string $photoProfil): static
-    {
-        $this->photoProfil = $photoProfil;
-
-        return $this;
-    }
-
-    public function getRole(): ?string
-    {
-        return $this->role;
-    }
-
-    public function setRole(string $role): static
-    {
-        $this->role = $role;
-
-        return $this;
-    }
-
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
     public function getPassword(): ?string
     {
         return $this->password;
@@ -89,6 +100,20 @@ class User implements \Stringable
         return $this;
     }
 
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials(): void
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    public function __toString(): string
+    {
+        return $this->email;
+    }
+
     public function getUsername(): ?string
     {
         return $this->username;
@@ -99,11 +124,5 @@ class User implements \Stringable
         $this->username = $username;
 
         return $this;
-    }
-
-    public function __toString()
-    {
-        // TODO: Implement __toString() method.
-        return $this->username;
     }
 }
