79da6abb24684dbe8a35b9e8a81d3d5f59d0f198
Dilion <antoine.eugenie@ynov.com>
2025-02-20 21:16:55 +0100

    detail

--- src/Controller/DetailController.php
+++ src/Controller/DetailController.php
@@ -2,15 +2,26 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 class DetailController extends AbstractController
 {
-    public function index(): Response
+    public function show(int $id, EntityManagerInterface $entityManager): Response
     {
-        $test = "chose";
-        return $this->render('detail.html.twig', ["test" => $test]);
+        // Récupérer l'article en fonction de l'ID
+        $article = $entityManager->getRepository(Article::class)->find($id);
+
+        // Si l'article n'existe pas, on renvoie une erreur 404
+        if (!$article) {
+            throw $this->createNotFoundException('L\'article avec l\'ID ' . $id . ' n\'existe pas.');
+        }
+        $user = $entityManager->getRepository(User::class)->find($article->getAuteurId());
+
+        // Afficher l'article dans une page dédiée
+        return $this->render('detail.html.twig', ['article' => $article, 'user' => $user]);
     }
 }
