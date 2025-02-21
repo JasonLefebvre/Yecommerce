4b73a83b20b83006c7b10507197a51103bbf57e8
JasonLefebvre <jasonlefebvre25septembre@gmail.com>
2025-02-21 14:44:55 +0100

    giga update de la mort qui tue

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,12 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 class AdminController extends AbstractController
 {
-    public function admin(): Response
+    public function admin(EntityManagerInterface $entityManager): Response
     {
         $user = $this->getUser();
 
@@ -15,8 +17,10 @@ class AdminController extends AbstractController
             return $this->redirectToRoute('app_login');
         }
 
+        $articles = $entityManager->getRepository(Article::class)->findAll();
+
         if (in_array("ROLE_ADMIN", $user->getRoles())) {
-            return $this->render('admin.html.twig', ['user' => $user]);
+            return $this->render('admin.html.twig', ['user' => $user, 'articles' => $articles]);
         } else {
             return $this->redirectToRoute('home');
         }
